df890676c65b7b78310f41314fe97dff64b3b1fc
Kurt Stephens <ks.github@kurtstephens.com>
2013-11-27 02:30:56 -0600

    Indent.

--- Rt/Cone.cc
+++ Rt/Cone.cc
@@ -13,36 +13,39 @@ Cone::Cone ( RtFloat HEIGHT, RtFloat RADIUS, RtFloat THETAMAX )
 {
 }
 
-int	Cone::isOn(const Point&P) const
+int Cone::isOn(const Point&P) const
 {
   return P.z >= 0.0 && P.z <= height && Quadric::isOn(P);
 }
 
-Point	Cone::P(const Param& p) {
-	angle	theta = utheta(p[0]);
-	scalar	rxy = radius * (1.0 - p[1]);
+Point Cone::P(const Param& p)
+{
+  angle theta = utheta(p[0]);
+  scalar rxy = radius * (1.0 - p[1]);
 
-	return Point(
-		rxy * thetax(theta),
-		rxy * thetay(theta),
-		p[1] * height);
+  return Point(
+               rxy * thetax(theta),
+               rxy * thetay(theta),
+               p[1] * height);
 }
 
-Param	Cone::p(const Point& p) {
-  return	Param(thetau(xytheta(p.x, p.y)), p.z / height);
-}
-
-
-Point	Cone::Ngp(const Param& p) {
-	angle	theta = utheta(p[0]);
-	return Point(
-		radius * thetax(theta),
-		radius * thetay(theta),
-		height);
+Param Cone::p(const Point& p)
+{
+  return Param(thetau(xytheta(p.x, p.y)), p.z / height);
 }
 
 
-Point	Cone::NgP(const Point& p)
+Point Cone::Ngp(const Param& p)
+{
+  angle theta = utheta(p[0]);
+  return Point(
+               radius * thetax(theta),
+               radius * thetay(theta),
+               height);
+}
+
+
+Point Cone::NgP(const Point& p)
 {
   Point P = p;
   ((Point2&) P).normalize() * radius;
@@ -50,62 +53,68 @@ Point	Cone::NgP(const Point& p)
   return P;
 }
 
-Point	Cone::Ng(RPI* p) {
-	return NgP(p->P());
+Point Cone::Ng(RPI* p)
+{
+  return NgP(p->P());
 }
 
 
-Point	Cone::dPdup(const Param& p) {
-	angle	theta = utheta(p.u);
-	RtFloat rxy = radius * thetamax.radians();
-
-	return Point(
-		rxy * dxdtheta(theta),
-		rxy * dydtheta(theta),
-		0.0 );
+Point Cone::dPdup(const Param& p)
+{
+  angle theta = utheta(p.u);
+  RtFloat rxy = radius * thetamax.radians();
+  
+  return Point(
+               rxy * dxdtheta(theta),
+               rxy * dydtheta(theta),
+               0.0 );
 }
 
-Point	Cone::dPdvp(const Param& p) {
-	angle	theta = utheta(p.u);
-	scalar	rxy = - radius;
-	return	Point (
-		rxy * thetax(theta),
-		rxy * thetay(theta),
-		height );
+Point Cone::dPdvp(const Param& p)
+{
+  angle theta = utheta(p.u);
+  scalar rxy = - radius;
+  return Point (
+                rxy * thetax(theta),
+                rxy * thetay(theta),
+                height );
 }
 
 inline
 Point
-Cone::random() const {
-	scalar	sr = 2.0 * radius;
-	Point	P;
-
-	do {
-		P = Point (
-			(RiRand() - 0.5) * sr,
-			(RiRand() - 0.5) * sr,
-			RiRand());
-	} while ( ((Point2&)P) % ((Point2&) P) > radius2 * P.z);
-
-	P.z = (1.0 - P.z) * height;
-	return P;
+Cone::random() const
+{
+  scalar sr = 2.0 * radius;
+  Point P;
+  
+  do {
+    P = Point (
+               (RiRand() - 0.5) * sr,
+               (RiRand() - 0.5) * sr,
+               RiRand());
+  } while ( ((Point2&)P) % ((Point2&) P) > radius2 * P.z);
+  
+  P.z = (1.0 - P.z) * height;
+  return P;
 }
 
 Point
-Cone::randomIn() {
-	return random();
+Cone::randomIn()
+{
+  return random();
 }
 
 Point
-Cone::randomOn() {
-	Point	P;
+Cone::randomOn()
+{
+  Point P;
 
-	do {
-		P = random();
-		((Point2&) P).normalize() * radius * (1.0 - (P.z / height));
-	} while ( ! isOn(P) );
-
-	return P;
+  do {
+    P = random();
+    ((Point2&) P).normalize() * radius * (1.0 - (P.z / height));
+  } while ( ! isOn(P) );
+  
+  return P;
 }
 
 
