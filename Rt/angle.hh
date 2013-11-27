548d071bc59f5dc606180f1c5501350d40f2a23f
Kurt Stephens <ks.github@kurtstephens.com>
2013-11-26 21:24:44 -0600

    angle is a double.

--- Rt/angle.hh
+++ Rt/angle.hh
@@ -14,23 +14,23 @@ class	angle {
 // Implement as radian, because C math library trig functions
 // expects them.
 //
-	scalar	r;
-	angle ( scalar a ) { r = a; }
-  static  angle _radians(scalar a) { return angle(a); }
-  static  angle _degrees(scalar a) { return angle(M_PI / 180.0 * a); }
+	double	r;
+	angle ( double a ) { r = a; }
+  static  angle _radians(double a) { return angle(a); }
+  static  angle _degrees(double a) { return angle(M_PI / 180.0 * a); }
 
 public:
 	angle ( ) {}
-	angle ( scalar y, scalar x ) { r = ::atan2((double) y, (double) x); }
+	angle ( double y, double x ) { r = ::atan2((double) y, (double) x); }
 
-  scalar number() const { return r; }
-  scalar radians() const { return r; }
-  scalar degrees() const { return 180.0 / M_PI * r; }
+  double number() const { return r; }
+  double radians() const { return r; }
+  double degrees() const { return 180.0 / M_PI * r; }
 
-	void	asin ( scalar y ) { r = ::asin ( (double) y ); }
-	void	acos ( scalar x ) { r = ::acos ( (double) x ); }
-	void	atan ( scalar y, scalar x ) { r = ::atan2 ( (double) y, (double) x ); }
-	void	atan ( scalar yx ) { r = ::atan( (double) yx ); }
+	void	asin ( double y ) { r = ::asin ( (double) y ); }
+	void	acos ( double x ) { r = ::acos ( (double) x ); }
+	void	atan ( double y, double x ) { r = ::atan2 ( (double) y, (double) x ); }
+	void	atan ( double yx ) { r = ::atan( (double) yx ); }
 
 	angle&	operator = ( const angle& l ) {
 		r = l.r;
@@ -52,15 +52,15 @@ friend	angle 	operator op ( const angle& L, const angle& R ) { \
 #undef OP
 
 #define	OP(op) \
-friend	angle	operator op ( const angle& L, scalar s ) { \
+friend	angle	operator op ( const angle& L, double s ) { \
 		return angle ( L.r op s ); } \
-friend	angle 	operator op ( scalar s, const angle& R ) { \
+friend	angle 	operator op ( double s, const angle& R ) { \
 		return angle ( s op R.r ); } \
 friend	angle 	operator op ( const angle& L, const angle& R ) { \
 		return angle ( L.r op R.r ); } \
 	angle&	operator op##= ( const angle& R ) { \
 		r op##= R.r; return *this; } \
-	angle&  operator op##= ( scalar s ) { \
+	angle&  operator op##= ( double s ) { \
 		r op##= s; return *this; }
 
 	OP(/)
@@ -81,26 +81,26 @@ friend	int	operator op ( const angle& L, const angle& R ) { \
 
 #undef	OP
 
-        friend angle degrees(scalar a);
-  friend angle degrees(scalar a);
-  friend angle radians(scalar a);
-  friend scalar to_radians(const angle &a);
-  friend scalar to_degrees(const angle &a);
-  friend scalar	sin (const angle &a );
-  friend scalar	cos (const angle &a );
-  friend scalar	tan (const angle &a );
+        friend angle degrees(double a);
+  friend angle degrees(double a);
+  friend angle radians(double a);
+  friend double to_radians(const angle &a);
+  friend double to_degrees(const angle &a);
+  friend double	sin (const angle &a );
+  friend double	cos (const angle &a );
+  friend double	tan (const angle &a );
 
 };
 
-inline angle degrees(scalar a) { return angle::_degrees(a); }
-inline angle radians(scalar a) { return angle::_radians(a); }
+inline angle degrees(double a) { return angle::_degrees(a); }
+inline angle radians(double a) { return angle::_radians(a); }
 
-inline scalar to_number(const angle &a) { return a.number(); }
-inline scalar to_radians(const angle &a) { return a.radians(); }
-inline scalar to_degrees(const angle &a) { return a.degrees(); }
-inline scalar	sin (const angle &a )  { return (scalar) ::sin ( (double) a.radians() ); }
-inline scalar	cos (const angle &a )  { return (scalar) ::cos ( (double) a.radians() ); }
-inline scalar	tan (const angle &a )  { return (scalar) ::tan ( (double) a.radians() ); }
+inline double to_number(const angle &a) { return a.number(); }
+inline double to_radians(const angle &a) { return a.radians(); }
+inline double to_degrees(const angle &a) { return a.degrees(); }
+inline double	sin (const angle &a )  { return (double) ::sin ( (double) a.radians() ); }
+inline double	cos (const angle &a )  { return (double) ::cos ( (double) a.radians() ); }
+inline double	tan (const angle &a )  { return (double) ::tan ( (double) a.radians() ); }
 
 
 inline std::ostream&	operator << ( std::ostream& os, const angle &x ) {
