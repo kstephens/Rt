17ebefb8d99c6393ab81b23113c1e03522bbca9f
Kurt Stephens <ks.github@kurtstephens.com>
2013-11-29 14:05:24 -0600

    More test Geometry.

---
+++ Rt/Geometry.hh
@@ -0,0 +1,13 @@
+#ifndef __Rt_GEOMETRY_HH
+#define __Rt_GEOMETRY_HH
+
+#include "Rt/Sphere.hh"
+#include "Rt/Cylinder.hh"
+#include "Rt/Cone.hh"
+#include "Rt/Disk.hh"
+
+// Extensions
+#include "Rt/Box.hh"
+#include "Rt/Plane.hh"
+
+#endif
